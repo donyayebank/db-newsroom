6c9498d6c563b8217976c77f92904b6cb7ec667a
donyayebank <donyayebank@gmail.com>
2026-06-28 19:39:15 +0330

    Update db-news-publisher.php

--- db-news-publisher/db-news-publisher.php
+++ db-news-publisher/db-news-publisher.php
@@ -7,17 +7,71 @@
  * Requires at least: 6.8
  * Requires PHP: 8.1
  * Author: DonyayeBank
+ * Author URI: https://donyayebank.ir
  * License: GPL-2.0+
  * Text Domain: db-news-publisher
  */
 
 defined('ABSPATH') || exit;
 
+/*
+|--------------------------------------------------------------------------
+| Plugin Constants
+|--------------------------------------------------------------------------
+*/
+
 define('DBNP_VERSION', '0.2.0');
-define('DBNP_PLUGIN_FILE', __FILE__);
-define('DBNP_PLUGIN_PATH', plugin_dir_path(__FILE__));
-define('DBNP_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('DBNP_FILE', __FILE__);
+define('DBNP_PATH', plugin_dir_path(__FILE__));
+define('DBNP_URL', plugin_dir_url(__FILE__));
 
-require_once DBNP_PLUGIN_PATH . 'includes/class-admin.php';
+/*
+|--------------------------------------------------------------------------
+| Load Classes
+|--------------------------------------------------------------------------
+*/
 
-new DBNP_Admin();
+require_once DBNP_PATH . 'includes/class-admin.php';
+require_once DBNP_PATH . 'includes/class-assets.php';
+require_once DBNP_PATH . 'includes/class-dashboard.php';
+require_once DBNP_PATH . 'includes/class-news.php';
+require_once DBNP_PATH . 'includes/class-settings.php';
+
+/*
+|--------------------------------------------------------------------------
+| Bootstrap Plugin
+|--------------------------------------------------------------------------
+*/
+
+final class DB_News_Publisher {
+
+    private static $instance = null;
+
+    public static function instance() {
+
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct() {
+
+        add_action('plugins_loaded', array($this, 'init'));
+
+    }
+
+    public function init() {
+
+        new DBNP_Admin();
+        new DBNP_Assets();
+        new DBNP_Dashboard();
+        new DBNP_News();
+        new DBNP_Settings();
+
+    }
+
+}
+
+DB_News_Publisher::instance();
