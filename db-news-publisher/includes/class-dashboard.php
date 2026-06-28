b9e2c62a6386a16d2f4b634e9b26047019973e1a
donyayebank <donyayebank@gmail.com>
2026-06-28 19:42:44 +0330

    Update class-dashboard.php

--- db-news-publisher/includes/class-dashboard.php
+++ db-news-publisher/includes/class-dashboard.php
@@ -1,1 +1,13 @@
+<?php
+defined('ABSPATH') || exit;
 
+class DBNP_Dashboard {
+
+    public function __construct() {
+
+        // نسخه 0.2.0
+        // فعلاً این کلاس فقط آماده توسعه است.
+
+    }
+
+}
