fbc7680ebd08541818d5120ee8edebcc9c3c3051
donyayebank <donyayebank@gmail.com>
2026-06-28 19:43:15 +0330

    Update class-news.php

--- db-news-publisher/includes/class-news.php
+++ db-news-publisher/includes/class-news.php
@@ -1,1 +1,13 @@
+<?php
+defined('ABSPATH') || exit;
 
+class DBNP_News {
+
+    public function __construct() {
+
+        // نسخه 0.2.0
+        // این کلاس مسئول مدیریت اخبار خواهد بود.
+
+    }
+
+}
