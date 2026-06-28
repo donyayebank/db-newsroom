d5cf89ad9e3801179ddff3f388cc246ca7a8f178
donyayebank <donyayebank@gmail.com>
2026-06-28 19:43:40 +0330

    Update class-settings.php

--- db-news-publisher/includes/class-settings.php
+++ db-news-publisher/includes/class-settings.php
@@ -1,1 +1,13 @@
+<?php
+defined('ABSPATH') || exit;
 
+class DBNP_Settings {
+
+    public function __construct() {
+
+        // نسخه 0.2.0
+        // این کلاس مسئول تنظیمات افزونه خواهد بود.
+
+    }
+
+}
