a39d8249449d9a6281e31f1b0b787bc001476beb
donyayebank <donyayebank@gmail.com>
2026-06-28 19:42:04 +0330

    Update class-assets.php

--- db-news-publisher/includes/class-assets.php
+++ db-news-publisher/includes/class-assets.php
@@ -1,26 +1,14 @@
 <?php
-/**
- * مدیریت فایل‌های CSS و JS
- *
- * @package DB_News_Publisher
- */
-
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 class DBNP_Assets {
 
-    /**
-     * Constructor
-     */
     public function __construct() {
 
-        add_action( 'admin_enqueue_scripts', array( $this, 'admin_assets' ) );
+        add_action('admin_enqueue_scripts', array($this, 'admin_assets'));
 
     }
 
-    /**
-     * بارگذاری فایل‌های پنل مدیریت
-     */
     public function admin_assets() {
 
         wp_enqueue_style(
@@ -33,7 +21,7 @@ class DBNP_Assets {
         wp_enqueue_script(
             'dbnp-admin',
             DBNP_URL . 'assets/js/admin.js',
-            array( 'jquery' ),
+            array('jquery'),
             DBNP_VERSION,
             true
         );
